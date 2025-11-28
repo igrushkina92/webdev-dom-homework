bd9b2c45381cddf0bcff07cdadbb2414f7d48cc6
igrushkina92 <a.igrushkina@mail.ru>
2025-11-28 13:31:09 +0300

    Выполнено дз на ветке hw-6

--- modules/initListeners.js
+++ modules/initListeners.js
@@ -57,9 +57,15 @@ export const initAddCommentListener = (renderComments) => {
       return;
     }
 
+    document.querySelector('.form-loading').style.display = 'block'
+    document.querySelector('.add-form').style.display = 'none'
 
     postComment(sanitizeHtml(text.value), sanitizeHtml(name.value)).then(
       (data) => {
+
+        document.querySelector('.form-loading').style.display = 'none'
+        document.querySelector('.add-form').style.display = 'flex'
+
         updateComments(data)
         renderComments()
           name.value = "";
